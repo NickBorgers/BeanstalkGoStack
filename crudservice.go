33bfae53c7f6c161627ed7418541a1d006d75bb5
Nick Borgers <nickborgers@gmail.com>
2019-06-01 21:34:03 +0000

    refactor crudservice

--- crudservice.go
+++ crudservice.go
@@ -8,24 +8,25 @@ import (
 )
 
 func main() {
-    port := os.Getenv("PORT")
+	port := os.Getenv("PORT")
         if port == "" {
-            port = "5000"
+	        port = "5000"
         }
 
-        http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-            if r.Method == "POST" {
-                if buf, err := ioutil.ReadAll(r.Body); err == nil {
-                    log.Printf("Received POST: %s\n", string(buf))
-                }
-            } else {
-		if buf, err := ioutil.ReadAll(r.Body); err == nil {
-                    log.Printf("Received GET: %s\n", string(buf))
-                }
-            }
-        })
+        http.HandleFunc("/pandas/", pandaRequestHandler);
 
         log.Printf("Listening on port %s\n\n", port)
         http.ListenAndServe(":"+port, nil)
 }
 
+func pandaRequestHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+        	if buf, err := ioutil.ReadAll(r.Body); err == nil {
+                	log.Printf("Received POST: %s\n", string(buf))
+                }
+        } else {
+                if buf, err := ioutil.ReadAll(r.Body); err == nil {
+    			log.Printf("Received GET: %s\n", string(buf))
+                }
+        }
+}
