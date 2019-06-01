3d7a37fa72f035973bd5618c31e8e79acd21cffc
Nick Borgers <nickborgers@gmail.com>
2019-06-01 22:19:03 +0000

    now sending requested panda names as message from FE

--- frontendservice.go
+++ frontendservice.go
@@ -4,13 +4,24 @@ import (
     "log"
     "net/http"
     "os"
+
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/sqs"
 )
+
+var svc *sqs.SQS
 
 func main() {
 	port := os.Getenv("PORT")
         if port == "" {
 	        port = "5000"
         }
+
+        sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	svc = sqs.New(sess)
 
         http.HandleFunc("/pandas/", pandaRequestHandler);
 
@@ -22,8 +33,27 @@ func pandaRequestHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		w.WriteHeader(http.StatusAccepted)
                	log.Printf("Received GET: %s\n", r.URL.Path)
+                sendMessage(r.URL.Path)
         } else {
 		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
     		log.Printf("Rejected %s: %s\n", r.Method, r.URL.Path)
         }
 }
+
+func sendMessage(name string) (*sqs.SendMessageOutput) {
+	// URL to our queue
+        qURL := "https://sqs.us-east-1.amazonaws.com/181171223360/testqueue"
+
+        result, err := svc.SendMessage(&sqs.SendMessageInput{
+  		MessageBody: aws.String(name),
+		QueueUrl:    &qURL,
+	})
+
+	if err != nil {
+		log.Printf("Error", err)
+		return nil
+	}
+
+	return result
+}
+
