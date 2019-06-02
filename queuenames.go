3463fd0955c667a6e6bd397933069ebd61d8b7f4
Nick Borgers <nickborgers@gmail.com>
2019-06-02 21:20:42 +0000

    fix missing import - thank you PR build hook

--- queuenames.go
+++ queuenames.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"os"
+)
+
 var requestQueue = "pandaDataRequest" + os.Getenv("ENV_NAME_MODIFIER")
 var healthDataQueue = "pandaHealthData" + os.Getenv("ENV_NAME_MODIFIER")
 var healthAnalysisQueue = "pandaHealthAnalysis" + os.Getenv("ENV_NAME_MODIFIER")
