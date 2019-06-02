b5b70475c11f04797e5521a39f68e9df4f873213
Nick Borgers <nickborgers@gmail.com>
2019-06-02 14:35:55 +0000

    really need to start using and building branches instead of this

--- dataservice.go
+++ dataservice.go
@@ -16,13 +16,14 @@ func main() {
 }
 
 func processPandaDataRequest(message *sqs.Message) {
-	pandaHealthData := getHealthDataForPandaByName(message.Body)
+	var requestedPandaName string = *message.Body
+	pandaHealthData := getHealthDataForPandaByName(requestedPandaName)
 	jsonPandaHealthData,err := json.Marshal(pandaHealthData)
 	if err == nil {
-		sendMessage(jsonPandaHealthData, healthDataQueue)
-		log.Printf("Retrieved and sent along data for requested panda %s", message.Body)
+		sendMessage(string(jsonPandaHealthData), healthDataQueue)
+		log.Printf("Retrieved and sent along data for requested panda %s", requestedPandaName)
 		deleteMessage(message, requestQueue)
 	} else {
-        	log.Printf("Could not build data for requested panda %s", message.Body)
+        	log.Printf("Could not build data for requested panda %s", requestedPandaName)
 	}
 }
